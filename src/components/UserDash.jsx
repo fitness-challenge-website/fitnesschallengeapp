d0bb6e38e7ac70811a3c7b9467d022266cb9098e
jreimer107 <jreimer107@gmail.com>
2019-03-26 21:46:15 -0500

    Working graphs!

--- src/components/UserDash.jsx
+++ src/components/UserDash.jsx
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import UserFeed from './UserFeed';
+import Chart from './Chart';
 class UserDash extends Component {
-    state = {};
-    render() {
-        return (
-            <div className='container'>
-                <UserFeed />
-            </div>
-        );
-    }
+	state = {};
+	render() {
+		return (
+			<div className='container'>
+				<Chart />
+				<UserFeed />
+			</div>
+		);
+	}
 }
 
 export default UserDash;
