a9ac3e2eb3b80c0f21e0c61ca39cf8283acde696
jkaplinsky1 <kaplinsky@wisc.edu>
2019-03-27 15:56:39 -0500

    Modal for Login
    Added a Modal for the login information. Added a few more packages including Bootstrap.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from "react"
 import "./App.css"
 import firebase from "firebase"
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import Modal from "react-responsive-modal";
+import Button from 'react-bootstrap/Button'
+
+
 
 firebase.initializeApp({
   apiKey: "AIzaSyAYdZGv2f-0gvWOyQ8zkk8HjbsJqmcKwOM",
@@ -29,23 +33,47 @@ class App extends Component {
     })
   }
 
+  state = {
+    open: false
+  };
+
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+
   render() {
+    const { open } = this.state;
+
     return (
       <div className="App">
-        <div className="SignIn">
-          {this.state.isSignedIn ? (
-            <span>
-              <div><h1>Signed In!</h1></div>
-              <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
-              <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
-            </span>
-          ) : (
-            <StyledFirebaseAuth
-              uiConfig={this.uiConfig}
-              firebaseAuth={firebase.auth()}
-            />
-          )}
-        </div>
+        <h1> Welcome to Fitness Challenge App! </h1>
+        <h3> version 1.0 </h3>
+
+{ /* ------------ LOGIN SECTION ------------ */ }
+        <Button varient="primary" onClick={this.onOpenModal}>Login</Button>
+        <Modal open={open} onClose={this.onCloseModal} center>
+          <h2>Login</h2>
+          <div className="SignIn">
+            {this.state.isSignedIn ? (
+              <span>
+                <div><h2>Signed In!</h2></div>
+                <h2>Welcome {firebase.auth().currentUser.displayName}</h2>
+                <Button variant="primary" onClick={() => firebase.auth().signOut()}>Sign out!</Button>
+              </span>
+            ) : (
+              <StyledFirebaseAuth
+                uiConfig={this.uiConfig}
+                firebaseAuth={firebase.auth()}
+              />
+            )}
+          </div>
+        </Modal>
+{ /* ------------ LOGIN SECTION ------------ */ }
+
       </div>
     )
   }
