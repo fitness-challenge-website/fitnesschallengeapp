32bd2a4283416fe7c757fafad9b1363f22991c2d
jkaplinsky1 <kaplinsky@wisc.edu>
2019-03-27 14:56:09 -0500

    Added Authentication
    Updated the project to use firebase and firebaseui for authentication. Enabled the authentication by email and authentication by google features.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,54 @@
-import React, { Component } from 'react';
-import './App.css';
-import UserDash from './components/UserDash';
+import React, { Component } from "react"
+import "./App.css"
+import firebase from "firebase"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAYdZGv2f-0gvWOyQ8zkk8HjbsJqmcKwOM",
+  authDomain: "fitness-challenge-app.firebaseapp.com"
+})
 
 class App extends Component {
-	render() {
-		return (
-			<div className='App'>
-				<UserDash />
-			</div>
-		);
-	}
+  state = { isSignedIn: false }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      console.log("user", user)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="SignIn">
+          {this.state.isSignedIn ? (
+            <span>
+              <div><h1>Signed In!</h1></div>
+              <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+              <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            </span>
+          ) : (
+            <StyledFirebaseAuth
+              uiConfig={this.uiConfig}
+              firebaseAuth={firebase.auth()}
+            />
+          )}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
