e9c490e8d7c1aeb1c7fc083a736e1ffac97b6b89
jkaplinsky1 <kaplinsky@wisc.edu>
2019-03-28 11:19:31 -0500

    Addition of a Homepage Dashboard and NavBar

---
+++ src/PageContent/MainDashBoard.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+import "../App.css"
+import firebase from "firebase"
+
+class MainDashBoard extends Component {
+  render() {
+    return (
+
+      <div className="App">
+        <h1> Welcome to the Fitness Challenge App</h1>
+        <h3> This is the temporary main page dashboard </h3>
+      </div>
+
+    )
+  }
+}
+
+export default MainDashBoard
