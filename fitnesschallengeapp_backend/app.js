e0adb039ebf42360bbc9db699df215455c259dfc
Jesse Brelje <jtbrelje@gmail.com>
2019-03-28 18:02:08 -0500

    testing some POST methods for activities

--- fitnesschallengeapp_backend/app.js
+++ fitnesschallengeapp_backend/app.js
@@ -9,9 +9,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 require('./server/routes')(app);
-app.get('*', (req, res) => res.status(200).send({
-  message: "Hello World"
-}));
+
+//app.get('*', (req, res) => res.status(200).send({
+//  message: "Hello World"
+//}));
 
 app.listen(3210, ()=>{
   console.log('Server @port 3210!');
