f32ae4a7b5aea48476bd15fab77d860b4898714e
jkaplinsky1 <kaplinsky@wisc.edu>
2019-04-07 22:50:39 -0500

    Test

---
+++ src/components/challengesFolder/yearly.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react"
+import firebase from "firebase"
+// import "./challenges.css";
+import { Button, Container, Row, Col } from 'react-bootstrap'
+
+class Yearly extends Component {
+  render() {
+    return (
+
+
+      <Container className="contentDiv">
+        <Row className="shadow-lg p-3 mb-5 bg-white">
+          <Col>
+            <h4>Yearly</h4>
+          </Col>
+        </Row>
+      </Container>
+
+
+    )
+  }
+}
+
+export default Yearly
