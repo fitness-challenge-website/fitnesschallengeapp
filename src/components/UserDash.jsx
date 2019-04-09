2091b3eb039dae5068280ce874c6632d0988fa6f
jreimer107 <jreimer107@gmail.com>
2019-04-08 22:52:30 -0500

    Unified activity format  with backend, implemented react-bootstrap syntax.

--- src/components/UserDash.jsx
+++ src/components/UserDash.jsx
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
 import UserFeed from './UserFeed';
 import Charts from './Charts';
+import { Container, Row } from 'react-bootstrap';
+import axios from 'axios';
+
 class UserDash extends Component {
 	state = {};
+
+	componentDidMount = () => {
+		axios
+			.post('http://localhost:3210/api/getUserStats', {
+				uid: this.state.uid,
+			})
+			.then(res => {
+				let data = res.data;
+				console.log(data);
+				// this.setState({
+				// 	username: data.username,
+				// 	email: data.email,
+				// 	f_name: data.f_name,
+				// 	l_name: data.l_name,
+				// 	height: data.height,
+				// 	weight: data.weight,
+				// 	age: data.age,
+				// });
+			})
+			.catch(err => {
+				alert('Check If you have a data in the user table.');
+				console.log(err);
+			});
+	};
+
 	render() {
 		return (
-			<div className='container'>
-				<div className='row'>
+			<Container>
+				<Row>
 					<Charts />
-				</div>
-				<div className='row'>
+				</Row>
+				<Row>
 					<UserFeed />
-				</div>
-			</div>
+				</Row>
+			</Container>
 		);
 	}
 }
