24c78964e482cfad22cc535eca6040409ef3f28e
Jesse Brelje <jbrelje@wisc.edu>
2019-04-11 12:25:01 -0500

    slight tweak to learnmore

--- src/Authentication/learnmore.jsx
+++ src/Authentication/learnmore.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react"
-// import firebase from "firebase"
-
+import Button from 'react-bootstrap/Button'
 
 class LearnMore extends Component {
   render() {
     return (
       <div className="App">
         <p>You've learned more!</p>
+        <Button variant="primary" href="/">
+            Back to Login
+        </Button>
       </div>
     )
   }
