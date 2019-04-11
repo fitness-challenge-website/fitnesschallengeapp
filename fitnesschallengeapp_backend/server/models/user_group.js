2b14ddc52aaf4212c75728ffd725cb2682954c0d
wcho8 <wcho8@wisc.edu>
2019-04-10 20:22:24 -0500

    Update models

--- fitnesschallengeapp_backend/server/models/user_group.js
+++ fitnesschallengeapp_backend/server/models/user_group.js
@@ -3,10 +3,8 @@ module.exports = (sequelize, DataTypes) => {
     ugid: {type:DataTypes.INTEGER,
           primaryKey: true,
           autoIncrement: true},
-    req_uid:DataTypes.STRING,
-    res_uid:DataTypes.STRING,
+    uid:DataTypes.STRING,
     gid: DataTypes.INTEGER,
-    status: DataTypes.STRING,
     joinAt: DataTypes.DATE
   }, {
     tableName: 'a_user_group',
@@ -22,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
     });
 
     User_Group.belongsTo(models.User, {
-      foreignKey: 'res_uid',
+      foreignKey: 'uid',
       as: 'User'
     });
   };
