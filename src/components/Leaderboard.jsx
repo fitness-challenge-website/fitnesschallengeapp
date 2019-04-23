f2b699c98d741804e1cc7d32018ad206aeed8d37
Lucia Muyolema <muyolema@wisc.edu>
2019-04-23 16:23:57 -0500

    sort leaderboard

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -20,7 +20,10 @@ class Leaderboard extends Component {
     super(props, context);
     this.state = {users: []
 
-  }};
+  };
+this.onSort=this.onSort.bind(this);
+this.onSortPoints=this.onSortPoints.bind(this);
+}
 
   componentDidMount() {
       axios.post('http://localhost:3210/api/listUsers')
@@ -32,6 +35,12 @@ class Leaderboard extends Component {
           })
   }
 
+  onSort(event, sortKey){
+    const users = this.state.users;
+    users.sort((a,b)=>a[sortKey].localeCompare(b[sortKey]))
+    this.setState({users})
+
+  }
   userList() {
     this.state.users.sort((a, b) => b.totalpoints - a.totalpoints);
       return this.state.users.map(function(currentUser, i){
@@ -39,22 +48,38 @@ class Leaderboard extends Component {
       })
   }
 
+  onSortPoints(event, sortKey){
+    const users = this.state.users;
+    users.sort((a, b) => b.totalpoints - a.totalpoints);
+    this.setState({users})
+  }
 
   render() {
+    var newusers=this.state.users;
 		return (
       <Container className="LeaderboardDisplay">
         <h1>Leaderboard</h1>
         <table className="table table-striped" style={{ marginTop: 20 }} >
             <thead>
                 <tr>
-                    <th>Username</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Total Points</th>
+                    <th onClick={e=>this.onSort(e, 'username')}>Username</th>
+                    <th onClick={e=>this.onSort(e, 'f_name')}>First Name</th>
+                    <th onClick={e=>this.onSort(e, 'l_name')}>Last Name</th>
+                    <th onClick={e=>this.onSortPoints(e, 'totalpoints')}>Total Points</th>
                 </tr>
             </thead>
             <tbody>
-                { this.userList() }
+              {newusers.map(function(currentUser, i){
+                return(
+                  <tr key={i} data-item={currentUser}>
+                    <td data-title="Username">{currentUser.username}</td>
+                    <td data-title="First Name">{currentUser.f_name}</td>
+                    <td data-title="Last Name">{currentUser.l_name}</td>
+                    <td data-title="Total Points">{currentUser.totalpoints}</td>
+                  </tr>
+
+                );
+              })}
             </tbody>
         </table>
       </Container>
